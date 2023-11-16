24f0dc8cc1801e59604ccf5127bc1caea9782c55
Matt Mangold <mmangold7@gmail.com>
2023-11-16 17:00:01 -0500

    cleaning things up a lot

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -7,35 +7,10 @@ namespace QuantumDiffusion3DFD.Client.Pages;
 public partial class Index
 {
     private QuantumSystem _quantumSystem;
-    private int _frameCount;
-    private double _frameRate;
     private (int x, int y, int z) dimensions = new(10, 10, 10);
+    private BoundaryType _boundaryType;
     private double _hbar = 1.0;
     private double _mass = 1.0;
-    private double _hbarSliderPosition = 0; // logarithmic scale
-    private double _massSliderPosition = 0; // logarithmic scale
-    private const double MinLogValue = -4; // Corresponds to 1/10000
-    private const double MaxLogValue = 4;  // Corresponds to 10000
-    private float _pointSize = 13.0f;
-    private CancellationToken simulationToken;
-    private CancellationTokenSource? simulationTokenSource;
-    private float _timeStep = 0.0002f;
-    private double _spaceStep = 0.1;
-
-    private double _logTimeStepPosition;
-    private double _logSpaceStepPosition;
-    private const double MinTimeLogValue = -5; // Corresponds to 10^-5 = 0.00001
-    private const double MaxTimeLogValue = -1; // Corresponds to 10^-1 = 0.1
-    private const double MinSpaceLogValue = -3; // Corresponds to 10^-3 = 0.001
-    private const double MaxSpaceLogValue = 1;  // Corresponds to 10^1 = 10
-
-    private double _originalTotalEnergy;
-    private double _currentTotalEnergy;
-
-    private double TimeStep => Math.Pow(10, _logTimeStepPosition);
-    private double SpaceStep => Math.Pow(10, _logSpaceStepPosition);
-
-    private BoundaryType _boundaryType;
 
     private double _gaussianX0;
     private double _gaussianY0;
@@ -44,12 +19,61 @@ public partial class Index
     private double _gaussianKx;
     private double _gaussianKy;
     private double _gaussianKz;
+    private double _originalTotalEnergy;
+    private double _currentTotalEnergy;
 
+    private float _pointSize = 13.0f;
+    private float _timeStep = 0.0002f;
+    private double _spaceStep = 0.1;
+    private double TimeStep => Math.Pow(10, _logTimeStepPosition);
+    private double SpaceStep => Math.Pow(10, _logSpaceStepPosition);
+
+    private int _frameCount;
+    private double _frameRate;
+    public static int MaxFrameRateMillis { get; set; } = 33;
     public bool Paused { get; set; } = true;
     public bool RunningSimulation { get; set; }
-    public static int SimulationDelayMilliseconds { get; set; } = 33;
 
-    private async Task UpdatePointSize(ChangeEventArgs e)
+    private CancellationToken? simulationToken;
+    private CancellationTokenSource? simulationTokenSource;
+    private bool _areControlsVisible = true;
+    private double _logTimeStepPosition;
+    private double _logSpaceStepPosition;
+    private double _hbarSliderPosition = 0; // logarithmic scale
+    private double _massSliderPosition = 0; // logarithmic scale
+    private const double MinPointLogValue = -4; // Corresponds to 1/10000
+    private const double MaxPointLogValue = 4;  // Corresponds to 10000
+    private const double MinTimeLogValue = -5; // Corresponds to 10^-5 = 0.00001
+    private const double MaxTimeLogValue = -1; // Corresponds to 10^-1 = 0.1
+    private const double MinSpaceLogValue = -3; // Corresponds to 10^-3 = 0.001
+    private const double MaxSpaceLogValue = 1;  // Corresponds to 10^1 = 10
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("initializeThreeJs",
+                new { x = dimensions.x, y = dimensions.y, z = dimensions.z }, _spaceStep);
+
+            await JSRuntime.InvokeVoidAsync("updatePointSize", _pointSize);
+        }
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _gaussianX0 = dimensions.x / 2;
+        _gaussianY0 = dimensions.y / 2;
+        _gaussianZ0 = dimensions.z / 2;
+        _gaussianSigma = 1.0;
+        _gaussianKx = 20;
+        _gaussianKy = 20;
+        _gaussianKz = 20;
+
+        await RestartSimulation();
+    }
+    private string ToggleText => _areControlsVisible ? "Hide Controls" : "Show Controls";
+
+    private async Task UpdateProbabilitySphereScale(ChangeEventArgs e)
     {
         _pointSize = float.Parse(e.Value.ToString());
         await JSRuntime.InvokeVoidAsync("updatePointSize", _pointSize);
@@ -88,42 +112,23 @@ public partial class Index
 
         simulationTokenSource = new CancellationTokenSource();
         simulationToken = simulationTokenSource.Token;
-        
+
         SetupQuantumSystem();
         await Update3DDisplay();
         StateHasChanged();
 
-        await StartQuantumSimulationLoop(simulationToken, Paused);
+        await StartQuantumSimulationLoop(simulationToken.Value, Paused);
     }
 
     private void TogglePause() => Paused = !Paused;
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            await JSRuntime.InvokeVoidAsync("initializeThreeJs", new { x = dimensions.x, y = dimensions.y, z = dimensions.z }, _spaceStep);
-            await JSRuntime.InvokeVoidAsync("updatePointSize", _pointSize);
-        }
-    }
-
-    protected override async Task OnInitializedAsync()
-    {
-        _gaussianX0 = dimensions.x / 2;
-        _gaussianY0 = dimensions.y / 2;
-        _gaussianZ0 = dimensions.z / 2;
-        _gaussianSigma = 1.0;
-        _gaussianKx = 20;
-        _gaussianKy = 20;
-        _gaussianKz = 20;
-
-        await RestartSimulation();
-    }
-
     private void SetupQuantumSystem()
     {
         _quantumSystem = new QuantumSystem(dimensions, _timeStep, _spaceStep, _boundaryType);
-        _quantumSystem.InitializeGaussianPacket(_gaussianX0, _gaussianY0, _gaussianZ0, _gaussianSigma, _gaussianKx, _gaussianKy, _gaussianKz);
+
+        _quantumSystem.InitializeGaussianPacket(
+            _gaussianX0, _gaussianY0, _gaussianZ0, _gaussianSigma, _gaussianKx, _gaussianKy, _gaussianKz);
+
         _originalTotalEnergy = _quantumSystem.CalculateTotalEnergy();
         _currentTotalEnergy = _quantumSystem.CalculateTotalEnergy();
     }
@@ -154,17 +159,13 @@ public partial class Index
                 StateHasChanged();
             }
 
-            var difference = SimulationDelayMilliseconds - elapsed.Milliseconds;
+            var difference = MaxFrameRateMillis - elapsed.Milliseconds;
             if (difference > 0)
                 await Task.Delay(difference, token);
             else
-                await Task.Delay(SimulationDelayMilliseconds, token);
+                await Task.Delay(MaxFrameRateMillis, token);
         }
     }
-
-    private bool _areControlsVisible = true; // Controls are hidden by default
-
-    private string ToggleText => _areControlsVisible ? "Hide Controls" : "Show Controls";
 
     private void ToggleControlsVisibility()
     {
